32fd69f04f8cb364f975da65b7eb044ab13128a6
Volodymyr Shynkar <volodymyr.shynkar@gmail.com>
2025-01-10 19:51:09 +0100

    change path for reports

--- jest-html-reporter.config.js
+++ jest-html-reporter.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    outputPath: './reports/test-report.html', // Path to generate the report
+    outputPath: './test-report.html', // Path to generate the report
     pageTitle: 'Accessibility Test Report',
     includeFailureMsg: true, // Include failure messages
     theme: 'defaultTheme',
